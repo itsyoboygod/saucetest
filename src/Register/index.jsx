ec60f792c826504a348571959bd53110aea8a69d
itsyoboygod <m.vini.ac@hotmail.com>
2021-02-22 16:29:05 -0300

    last update = Igloo - 22/02 - 16:30

--- src/Register/index.jsx
+++ src/Register/index.jsx
@@ -1,45 +1,67 @@
-import { React, Component } from "react";
+import { React, Component, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./style.css";
 
-export default class Registerpanel extends Component {
-  render() {
-    return (
-      <div className="loginpanel-div">
-        <div className="loginpanel-left">
-          <h1>START FROM THE TOP !!!</h1>
-        </div>
-        <div className="loginpanel-right">
-          <h1>Register</h1>
-          <form>
-            <input
-              type="text"
-              placeholder="Email"
-              className="text-input"
-            ></input>
-            <input
-              type="text"
-              placeholder="Username"
-              className="text-input"
-            ></input>
-            <div className="radio-input">
-              <input type="radio" name="gender" id="radio-input-id" /> Male
-              <input type="radio" name="gender" /> Female
-              <input type="radio" name="gender" /> Other
-            </div>
-            <input
-              type="password"
-              placeholder="Password"
-              className="text-input"
-            ></input>
-            <button>Register</button>
-          </form>
-          <p>
-            Already a member? <Link to={`/Login`}>Login</Link>
-          </p>
-        </div>
+const Registerpanel = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const registerForm = (e) => {
+    console.log(e.target.value);
+    e.preventDefault();
+  };
+
+  const registerUser = (e) => {
+    alert("User registred " + setEmail + setUsername + setGender);
+    registerForm.e.target = "";
+  };
+
+  return (
+    <div className="loginpanel-div">
+      <div className="loginpanel-left">
+        <h1>START FROM THE TOP !!!</h1>
       </div>
-    );
-  }
-}
+      <div className="loginpanel-right">
+        <h1>Register</h1>
+        <form onChange={registerForm}>
+          <input type="text" placeholder="Email" className="text-input"></input>
+          <input
+            type="text"
+            placeholder="Username"
+            className="text-input"
+            name="email"
+          ></input>
+          <div className="radio-input">
+            <input type="radio" name="gender" id="id-radio-input-male" /> Male
+            <input type="radio" name="gender" id="id-radio-input-female" />{" "}
+            Female
+            <input type="radio" name="gender" id="id-radio-input-other" /> Other
+          </div>
+          <input
+            type="password"
+            placeholder="Password"
+            className="text-input"
+            name="password"
+          ></input>
+          {/* <select>
+            <option>test 1</option>
+            <option>test 2</option>
+            <option>test 3</option>
+            <option>test 4</option>
+          </select> */}
+          <button type="submit" onClick={registerUser}>
+            Register
+          </button>
+        </form>
+        <p>
+          Already a member? <Link to={`/Login`}>Login</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Registerpanel;
